1fe3427c51432a1d3f2cbd1344739c87cab77a2b
dhunnapotha <gautam4you@gmail.com>
2015-05-26 08:35:48 +0530

    Added one more scenario in self_in_blocks. That tests self inside yields

--- self_in_blocks.rb
+++ self_in_blocks.rb
@@ -13,6 +13,10 @@ class MyClass
     instance_eval(&block)
   end
 
+  def check_self_inside_yield
+    yield
+  end
+
 
 end
 
@@ -21,6 +25,9 @@ myObj = MyClass.new
 
 puts self #main
 myObj.run_block(p) #main
+MyClass.new.check_self_inside_yield{puts self} #main
+
 myObj.instance_eval_block(p) ##<MyClass:0x00000006ab13e0> or something similar, which is the object instantiated
 MyClass.class_eval(&p) #MyClass
 MyClass.class_instance_eval(p) #MyClass
+
